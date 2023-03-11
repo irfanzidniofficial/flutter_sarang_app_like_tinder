98fdc8d0dd3fcbece295d4873af2b1188c04e33e
irfanzidniofficial <irfanzidniofficial@gmail.com>
2023-03-11 21:24:21 +0700

    add data user

--- lib/src/features/like_you/data/data_user_dummy.dart
+++ lib/src/features/like_you/data/data_user_dummy.dart
@@ -26,4 +26,36 @@ List<User> dataUserDummy = [
         'I want to have big business and create a great impact for the people, finance, health, and many good things in this world.',
     imagePath: '${AssetImageIconManager.assetPath}/people_love3_image.png',
   ),
+  User(
+    fullName: 'Aiyla',
+    age: 21,
+    occupation: 'Lecturer',
+    description:
+        'I have been teaching for 2 years, and I am  passionate about sharing knowledge and helping my students succeed.',
+    imagePath: '${AssetImageIconManager.assetPath}/people_love4_image.png',
+  ),
+  User(
+    fullName: 'Azqila',
+    age: 24,
+    occupation: 'Artist',
+    description:
+        'I specialize in actress, and I have been creating art for 6 years. I have exhibited my work in various film exhibitions ',
+    imagePath: '${AssetImageIconManager.assetPath}/people_love5_image.png',
+  ),
+  User(
+    fullName: 'Azqila',
+    age: 20,
+    occupation: 'Student',
+    description:
+        'I am a female medical student. I am currently studying to become a doctor, and I am passionate about helping others',
+    imagePath: '${AssetImageIconManager.assetPath}/people_love6_image.png',
+  ),
+  User(
+    fullName: 'Harika',
+    age: 21,
+    occupation: 'Youtuber',
+    description:
+        'I create content related to vlog, and I have been doing so for 5 years. My channel focuses on food review, and I love creating videos that entertain',
+    imagePath: '${AssetImageIconManager.assetPath}/people_love7_image.png',
+  ),
 ];
